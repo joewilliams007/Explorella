b7daa7a77ba3468ca4027f02930ef03a9d1e369e
joewilliams007 <williamsthehack@gmail.com>
2024-11-13 21:46:08 +0100

    The screen transition animations are now fast.

--- composeApp/src/commonMain/kotlin/com/app/explorella/navigation/graphs/RootNavGraph.kt
+++ composeApp/src/commonMain/kotlin/com/app/explorella/navigation/graphs/RootNavGraph.kt
@@ -1,5 +1,8 @@
 package com.app.explorella.navigation.graphs
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
@@ -19,6 +22,10 @@ fun RootNavGraph(
     NavHost(
         navController = rootNavController,
         startDestination = Graph.NAVIGATION_BAR_SCREEN_GRAPH,
+        enterTransition = { fadeIn(initialAlpha = 0f, animationSpec = tween(durationMillis = 200)) },
+        exitTransition = { fadeOut() },
+        popEnterTransition = { fadeIn(initialAlpha = 0f, animationSpec = tween(durationMillis = 200)) },
+        popExitTransition = { fadeOut() }
     ) {
         mainNavGraph(rootNavController = rootNavController, innerPadding = innerPadding, sqlDriver = sqlDriver)
         composable(
