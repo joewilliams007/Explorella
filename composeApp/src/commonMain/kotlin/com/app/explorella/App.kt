84e1ba6535d23d29148d7e46d36cc474419f5b4c
joewilliams007 <williamsthehack@gmail.com>
2024-11-14 01:33:00 +0100

    Added theme colors & dark mode (follows system), added predictive back gesture, sped up the page animations. Removed material (not 3) dependency as material3 is in use.

--- composeApp/src/commonMain/kotlin/com/app/explorella/App.kt
+++ composeApp/src/commonMain/kotlin/com/app/explorella/App.kt
@@ -1,14 +1,14 @@
 package com.app.explorella
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import app.cash.sqldelight.db.SqlDriver
+import com.app.explorella.ui.theme.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App(sqlDriver: SqlDriver) {
-    MaterialTheme {
+    AppTheme {
         MainScreen(sqlDriver)
     }
 }
