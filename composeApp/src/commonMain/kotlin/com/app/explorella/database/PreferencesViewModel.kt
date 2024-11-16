b914731f2aa835a5da95917aaa0cf16168cdbfcc
joewilliams007 <williamsthehack@gmail.com>
2024-11-16 19:30:43 +0100

    Added queries. Added preferences table.

---
+++ composeApp/src/commonMain/kotlin/com/app/explorella/database/PreferencesViewModel.kt
@@ -0,0 +1,34 @@
+package com.app.explorella.database
+
+import androidx.lifecycle.ViewModel
+import app.cash.sqldelight.db.SqlDriver
+import com.app.explorella.BucketItem
+import com.app.explorella.Preferences
+import com.app.explorella.createDatabase
+
+class PreferencesViewModel(sqlDriver: SqlDriver) : ViewModel() {
+    private val database = createDatabase(sqlDriver)
+    private val queries = database.databaseQueries
+
+    /**
+     * Set a preference value.
+     */
+    fun setPreference(preferenceKey: String, preferenceValue: String) {
+        database.transaction {
+            val existingPreference = queries.getPreference(preferenceKey).executeAsOneOrNull()
+
+            if (existingPreference != null) {
+                queries.setPreference(preference_value = preferenceKey, preference_key = preferenceValue)
+            } else {
+                queries.createPreference(preference_key = preferenceKey, preference_value = preferenceValue)
+            }
+        }
+    }
+
+    /**
+     * Get a preference.
+     */
+    fun getPreference(preferenceKey: String): Preferences {
+        return queries.getPreference(preferenceKey).executeAsOne()
+    }
+}
