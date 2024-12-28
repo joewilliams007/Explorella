d18fca1dfff70652620c7d5955bb3e423911b65d
ColorfulBisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>
2024-12-28 23:40:37 +0100

    Added basic Entry display.
    Signed-off-by: ColorfulBisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>

--- composeApp/src/commonMain/kotlin/com/app/explorella/screens/Timeline.kt
+++ composeApp/src/commonMain/kotlin/com/app/explorella/screens/Timeline.kt
@@ -1,23 +1,29 @@
 package com.app.explorella.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import app.cash.sqldelight.db.SqlDriver
+import com.app.explorella.database.BucketViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,5 +48,21 @@ fun TimelineScreen(
             }
         )
         Spacer(modifier = Modifier.height(20.dp))
+
+        DisplayPage(sqlDriver)
+    }
+}
+
+@Composable
+fun DisplayPage(sqlDriver: SqlDriver) {
+    val bucket = remember { BucketViewModel(sqlDriver = sqlDriver) }
+    val bucketEntries = remember { mutableStateOf(bucket.getIncompleteBucketEntries()) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(bucketEntries.value) { entry ->
+                Text(text = entry.title, modifier = Modifier.padding(16.dp))
+            }
+        }
     }
 }
