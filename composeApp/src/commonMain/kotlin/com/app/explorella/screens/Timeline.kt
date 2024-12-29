fb0b70bca5f3e445da2a9ba2ad5df0dd55ce68b2
ColorfulBisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>
2024-12-29 16:49:40 +0100

    Added Dropdown Menu.
    Signed-off-by: ColorfulBisuketto <72705023+ColorfulBisuketto@users.noreply.github.com>

--- composeApp/src/commonMain/kotlin/com/app/explorella/screens/Timeline.kt
+++ composeApp/src/commonMain/kotlin/com/app/explorella/screens/Timeline.kt
@@ -1,18 +1,17 @@
 package com.app.explorella.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -22,7 +21,10 @@ import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -30,18 +32,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import app.cash.sqldelight.db.SqlDriver
 import com.app.explorella.BucketItem
 import com.app.explorella.database.BucketViewModel
-import com.app.explorella.navigation.Routes
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -74,11 +78,13 @@ fun TimelineScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DisplayPage(rootNavController: NavController?, sqlDriver: SqlDriver) {
+fun DisplayPage(rootNavController: NavController, sqlDriver: SqlDriver) {
     val bucket = remember { BucketViewModel(sqlDriver = sqlDriver) }
     val bucketEntries = remember { mutableStateOf(emptyList<BucketItem>()) }
     val isAscending = rememberSaveable { mutableStateOf(true) }
+    val focusRequester = remember { FocusRequester() }
 
     // Fetch and sort data
     LaunchedEffect(Unit) {
@@ -107,19 +113,27 @@ fun DisplayPage(rootNavController: NavController?, sqlDriver: SqlDriver) {
     } else {
         Column(modifier = Modifier.fillMaxSize()) {
             // Sorting Button
-            IconButton(
-                onClick = {
-                    isAscending.value = !isAscending.value
-                    bucketEntries.value = bucketEntries.value
-                        .sortedBy { if (isAscending.value) it.timestamp else -it.timestamp!! }
-                },
-                modifier = Modifier.padding(16.dp)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        isAscending.value = !isAscending.value
+                        bucketEntries.value = bucketEntries.value
+                            .sortedBy { if (isAscending.value) it.timestamp else -it.timestamp!! }
+                    },
+                verticalAlignment = Alignment.CenterVertically,
             ) {
                 Icon(
                     imageVector = if (isAscending.value) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
                     contentDescription = "Sort Order",
                     modifier = Modifier.size(32.dp),
                     tint = MaterialTheme.colorScheme.primary
+                )
+                Text(
+                    text = if (isAscending.value) "Asc" else "Desc",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.primary
                 )
             }
 
@@ -148,73 +162,98 @@ fun DisplayPage(rootNavController: NavController?, sqlDriver: SqlDriver) {
                             modifier = Modifier.fillMaxWidth(),
                             verticalAlignment = Alignment.CenterVertically
                         ) {
-                            // Timeline: Line and Point (Dotted line)
-                            Box(
-                                modifier = Modifier
-                                    .width(40.dp) // Width for the timeline line
-                                    .fillMaxHeight(), // Ensure Box takes up full height
-                                contentAlignment = Alignment.TopCenter
-                            ) {
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxHeight()
-                                ) {
-                                    // Add dots to represent points in the timeline
-                                    Icon(
-                                        imageVector = Icons.Default.CheckCircle,
-                                        contentDescription = "Timeline Point",
-                                        modifier = Modifier.size(16.dp),
-                                        tint = MaterialTheme.colorScheme.primary
-                                    )
-                                }
-                            }
-
-                            // Card Content
-                            Card(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(start = 8.dp),
-                                elevation = CardDefaults.cardElevation(4.dp),
-                                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
-                            ) {
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(16.dp),
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    Column(modifier = Modifier.weight(1f)) {
-                                        Text(
-                                            text = entry.title,
-                                            style = MaterialTheme.typography.titleMedium,
-                                            fontWeight = FontWeight.Bold,
-                                            color = MaterialTheme.colorScheme.onSurface
-                                        )
-                                        Text(
-                                            text = formatTimestamp(entry.timestamp!!),
-                                            style = MaterialTheme.typography.bodySmall,
-                                            color = MaterialTheme.colorScheme.onSurfaceVariant
-                                        )
-                                    }
-                                    IconButton(onClick = {
-                                        rootNavController?.currentBackStackEntry?.savedStateHandle?.apply {
-                                            set("itemId", entry.id)
-                                        }
-                                        rootNavController?.navigate(Routes.ItemDetail.route)
-                                    }) {
-                                        Icon(
-                                            imageVector = Icons.Default.MoreVert,
-                                            contentDescription = "More Options",
-                                            tint = MaterialTheme.colorScheme.onSurfaceVariant
-                                        )
-                                    }
-                                }
-                            }
+                            Icon(
+                                imageVector = Icons.Default.CheckCircle,
+                                contentDescription = "Timeline Point",
+                                modifier = Modifier.size(16.dp),
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                            TimeCard(rootNavController, entry, focusRequester)
                         }
                     }
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimeCard(rootNavController: NavController, entry: BucketItem, focusRequester: FocusRequester) {
+    // Card Content
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = entry.title,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+                Text(
+                    text = formatTimestamp(entry.timestamp!!),
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+
+            // Dropdown Menu for More Options
+            DropDownMenu(entry.id)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropDownMenu(id: Long) {
+    val expanded = rememberSaveable { mutableStateOf(false) }
+
+    IconButton(
+        onClick = {
+            expanded.value = true
+        },
+
+        ) {
+        Icon(
+            imageVector = Icons.Default.MoreVert,
+            contentDescription = "More Options",
+            tint = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+
+    DropdownMenu(
+        expanded = expanded.value,
+        onDismissRequest = { expanded.value = false }
+    ) {
+        DropdownMenuItem(
+            text = { Text("Details") },
+            onClick = {
+                expanded.value = false
+            }
+        )
+        DropdownMenuItem(
+            text = { Text("Add Picture") },
+            onClick = {
+                expanded.value = false
+            }
+        )
+        DropdownMenuItem(
+            text = { Text("Delete") },
+            onClick = {
+                expanded.value = false
+            }
+        )
     }
 }
 
