1f0485f48eec9ab05ba00b6a8bae1f1a8522de8e
jpatulad <jpatulad@cisco.com>
2024-12-30 22:43:17 +0100

    -bucket screen added

--- composeApp/src/commonMain/kotlin/com/app/explorella/screens/ViewBuckets.kt
+++ composeApp/src/commonMain/kotlin/com/app/explorella/screens/ViewBuckets.kt
@@ -1,24 +1,17 @@
 package com.app.explorella.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -26,18 +19,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
 import app.cash.sqldelight.db.SqlDriver
 import com.app.explorella.database.BucketViewModel
 import com.app.explorella.navigation.Routes
+import androidx.compose.material3.Checkbox
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -113,13 +108,8 @@ fun ViewBucketScreen(
                         items(bucketList) { e ->
                             ElevatedCard(
                                 modifier = Modifier
-                                    .fillMaxWidth().clickable {
-                                        rootNavController.currentBackStackEntry?.savedStateHandle?.apply {
-                                            set("itemId", e.id)
-                                        }
-                                        rootNavController.navigate(Routes.ItemDetail.route)},
+                                    .fillMaxWidth(),
                                 shape = RoundedCornerShape(12.dp)
-
                             ) {
                                 Row(
                                     modifier = Modifier
@@ -147,12 +137,17 @@ fun ViewBucketScreen(
                                     )
                                     Row(
                                         verticalAlignment = Alignment.CenterVertically,
+                                        modifier = Modifier.weight(1f).clickable {
+                                            rootNavController.currentBackStackEntry?.savedStateHandle?.apply {
+                                                set("itemId", e.id)
+                                            }
+                                            rootNavController.navigate(Routes.ItemDetail.route)
+                                        }
                                     ) {
                                         Text(
                                             text = e.title,
                                             style = MaterialTheme.typography.titleMedium,
-                                            color = MaterialTheme.colorScheme.onSurface,
-                                            modifier = Modifier.weight(1f)
+                                            color = MaterialTheme.colorScheme.onSurface
                                         )
                                     }
                                     Icon(
